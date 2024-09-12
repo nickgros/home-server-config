ccaff3762a5f57dc0b039c0f709e786cc8c1e9ad
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-09-12 09:36:08 -0400

    Split the docker-compose files

--- backup-to-syncthing.sh
+++ backup-to-syncthing.sh
@@ -1,12 +1,11 @@
 SERVER_CONFIG_DIR=$HOME/home-server-config
 
-sudo tar --exclude="\'$SERVER_CONFIG_DIR/etc-syncthing/home-server-backup\'" -czvf \
-        "$SERVER_CONFIG_DIR/etc-syncthing/home-server-backup/backup.tar.gz" \
+sudo tar --exclude="\'$SERVER_CONFIG_DIR/syncthing/data/home-server-backup\'" -czvf \
+        "$SERVER_CONFIG_DIR/syncthing/data/home-server-backup/backup.tar.gz" \
         "$SERVER_CONFIG_DIR/.env" \
-        "$SERVER_CONFIG_DIR/etc-dnsmasq.d" \
-        "$SERVER_CONFIG_DIR/etc-home-assistant" \
-        "$SERVER_CONFIG_DIR/etc-mosquitto" \
-        "$SERVER_CONFIG_DIR/etc-pgdata" \
-        "$SERVER_CONFIG_DIR/etc-pihole" \
-        "$SERVER_CONFIG_DIR/etc-syncthing/config" \
-        "$SERVER_CONFIG_DIR/etc-zigbee2mqtt"
+        "$SERVER_CONFIG_DIR/pihole/data" \
+        "$SERVER_CONFIG_DIR/homeassistant/data" \
+        "$SERVER_CONFIG_DIR/mosquitto/data" \
+        "$SERVER_CONFIG_DIR/postgres/data" \
+        "$SERVER_CONFIG_DIR/syncthing/data/config" \
+        "$SERVER_CONFIG_DIR/zigbee2mqtt/data"
